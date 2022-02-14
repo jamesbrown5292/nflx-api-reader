f89ade322ae8138b8d21a8c6b2df2cf042517589
James Brown <james-dev@jamh.ai>
2022-02-14 09:52:27 -0700

    async operations tweaks

--- index.js
+++ index.js
@@ -2,54 +2,123 @@ import * as apiTrackers from './utils/apiTracker.js'
 import axios from 'axios'
 import genres from './apiResults/genres.js'
 import dotenv from 'dotenv'
+import * as fs from 'fs';
+
 dotenv.config()
 
-const {returnThis, apiCallCount, getApiCallCount, incrementApiCallCount} = apiTrackers.apiTrackers
+const { returnThis, apiCallCount, getApiCallCount, incrementApiCallCount } = apiTrackers.apiTrackers
 
 
 const unogsNGptions = {
     method: 'GET',
     url: 'https://unogsng.p.rapidapi.com/search',
     params: {
-      newdate: '1989-02-01',
-      genrelist: '',
-      type: 'series',
-      start_year: '1972',
-      orderby: 'title',
-      limit: '100',
-      subtitle: 'english',
-      audio: 'english',
-      offset: '0',
-      end_year: '2022'
+        newdate: '1989-02-01',
+        genrelist: '',
+        type: 'series',
+        start_year: '1972',
+        orderby: 'title',
+        limit: '100',
+        subtitle: 'english',
+        audio: 'english',
+        offset: '0',
+        end_year: '2022'
     },
     headers: {
-      'x-rapidapi-host': 'unogsng.p.rapidapi.com',
-      'x-rapidapi-key': process.env.XRAPIDAPIKEY
+        'x-rapidapi-host': 'unogsng.p.rapidapi.com',
+        'x-rapidapi-key': process.env.XRAPIDAPIKEY
     }
 };
 
-getApiCallCount().then(res => {
-    let apiCalls = res
-    console.log("Api calls is:", apiCalls)
-}).then(() => {
-    let res;
-    Object.keys(genres).forEach((idCode, apiCalls) => {
-        if (apiCalls < 100) {
-            console.log("API CALLS IS", apiCalls)
-            console.log("Simulated API Call, write data to file")
-            res = 100;
-            console.log("res length is", res, "\n query api again")
-            if (res == 100) {
-                console.log("More results available, query again")
-                console.log("Simulating Api Call")
-                res--;
-            }
 
-        }
-        incrementApiCallCount()
+const asyncTest = async () => {
+    for (let i = 0; i < 10; i++) {
+        await getApiCallCount()
+        // await incrementApiCallCount()
+    };
+
+    console.log("Loop finished")
+}
+
+// asyncTest()
+
+
+
+function waitforme(milisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, milisec);
     })
+}
 
-})
+async function printy() {
+    for (let i = 0; i < 10; ++i) {
+        await waitforme(1000);
+        console.log(i);
+    }
+    console.log("Loop execution finished!)");
+}
+
+
+// printy();
+
+const readAPICount = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./apiTrackers/apiCalls.txt', 'utf-8', (e, data) => {
+            e && reject(e) || resolve(data)
+        })
+    })
+}
+
+const incrementApiCount = async () => {
+    await readAPICount().then((res) => {
+        console.log("incrementing...")
+        console.log("res is...", res)
+        const countIncremented = parseInt(res) + 1
+        return new Promise((resolve, reject) => {
+            fs.writeFile('./apiTrackers/apiCalls.txt', `${countIncremented}`, (e) => {
+                e && reject(e) || resolve(countIncremented)
+            })
+        })
+    })
+}
+
+// const loopRead = async () => {
+//     for (let i = 0; i < 10; ++i) {
+//         await readAPICount().then((res) => {
+//             console.log("reading count:", res)
+//         }).then(() => {
+//             console.log("incrementing api count")
+//             incrementApiCount()
+//         }).then(() => {
+//             console.log("i is", i);
+//         })
+//         // incrementApiCount();
+//     }
+//     console.log("Loop execution finished!");
+// }
+
+// loopRead();
+
+
+
+const testLoop = async () => {
+    let i = 0;
+    for (const key of Object.keys(genres)) {
+        if (i < 100) {
+            await apiCallCount()
+                .then((res) => {
+                    console.log("Iteration key", key)
+                    incrementApiCallCount(res)
+                }).catch((e) => {
+                    console.log("Promise error:", e)
+                })
+            i++;
+        }
+    }
+
+}
+testLoop();
+
 // console.log("apitracker", apiTrackers)
 // console.log("API", returnThis())
 //Cycle through the list of genres - loop over object.keys
--- utils/apiTracker.js
+++ utils/apiTracker.js
@@ -2,29 +2,38 @@ import * as fs from 'fs';
 
 const apiTrackers = {
 
-    apiCallCount: function() {
+    apiCallCount: function () {
         return new Promise((resolve, reject) => {
             fs.readFile('./apiTrackers/apiCalls.txt', 'utf-8', (e, data) => {
                 e && reject(e) || resolve(data)
-           })
-        })
-    },
-
-    getApiCallCount: async function() {
-        let apiCount = await apiTrackers.apiCallCount()
-        return parseInt(apiCount)
-    },
-
-    incrementApiCallCount: function() {
-        apiTrackers.getApiCallCount().then((res) => {
-            let apiCountIncremented = parseInt(res) + 1;
-            fs.writeFile('./apiTrackers/apiCalls.txt', `${apiCountIncremented}`, (e) => {
-                e && console.error(e)
             })
         })
-    }
+    },
 
+
+    getApiCallCount: function () {
+        apiTrackers.apiCallCount().then((res) => {
+            console.log("REturning:", res)
+            return parseInt(res)
+        })
+    },
+
+    incrementApiCallCount: async function (count) {
+        const incrementAPI = parseInt(count) + 1
+        new Promise((reject, resolve) => {
+            console.log("Writing file with ", incrementAPI)
+            fs.writeFile('./apiTrackers/apiCalls.txt', `${incrementAPI}`, (err) => {
+                if (err) throw err;
+            })
+        })
+
+
+        // let apiCountIncremented = parseInt(res) + 1;
+        // fs.writeFile('./apiTrackers/apiCalls.txt', `${apiCountIncremented}`, (e) => {
+        //     e && console.error(e) || console.log("written to file", apiCountIncremented)
+        // })
+    }
 }
 
-export {apiTrackers}
+export { apiTrackers }
 
